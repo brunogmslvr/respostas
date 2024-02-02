6c364c9beb808f3ecf905b395f4c3b96fd82b5da
901196 <901196@PITLAB2178770.senac.educ>
2024-02-02 11:54:05 -0300

    EXERCICO REVISÃO

---
+++ revisao_2024/src/revisao_2024/Revisao_2024.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package revisao_2024;
+
+/**
+ *
+ * @author 901196
+ */
+public class Revisao_2024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ex1
+        System.out.println("hello world");
+        
+        // This is a single-line comment
+        /* This is a multi-line comment */
+        
+        //ex3
+        String carname = "Volve";
+        
+        // ex4
+        int maxSpeed = 120;
+        
+        // ex5
+        int x =5;
+        int y =10;
+        System.out.println(x + y);
+        
+        // ex6
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        System.out.println(z);
+        
+        
+        
+    }
+    
+}
---
+++ revisao_2024/src/revisao_2024/Revisao_2024_1.java
@@ -0,0 +1,140 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package revisao_2024;
+
+/**
+ *
+ * @author 901196
+ */
+public class Revisao_2024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ex1
+        System.out.println("hello world");
+        
+        // This is a single-line comment
+        /* This is a multi-line comment */
+        
+        //ex3
+        String carname = "Volve";
+        
+        // ex4
+        int maxSpeed = 120;
+        
+        // ex5
+        int x =5;
+        int y =10;
+        System.out.println(x + y);
+        
+        // ex6
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        System.out.println("a soma é:"+z);
+        
+        // ex7
+        int x=5; y=6; z=50;
+        System.out.println(x+y+z);
+        
+        // ex8
+        int myNum = 9;
+        float myFloatnum = 8,99f
+        char myLetter = 'A';
+        boolean myBool = false
+        String myText = "Hello World";
+        
+        //ex9
+        primitive.data types
+        
+        //ex10
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
+        
+        //x11
+        System.out.println("A multiplicação é: " + (10 * 5));
+        
+        //x12
+        System.out.println("A divisão é: " + (10 / 5));
+        
+        //x13
+         x = 10;
+         ++x;
+         
+         //x14
+         int x = 10;
+         x += 5;
+         
+         //x15
+         greeting = Hello;
+         print(greeting);
+         
+         //x16
+         String txt = "Hello";
+         String txtMaiusculo = txt.toUpperCase();
+         System.out.println(txtMaiusculo);
+         
+         //17
+         String firstName = "John";
+         String lastName = "Doe";
+         System.out.println(firstName+lastName);
+         
+         //18
+         String firstName = "John";
+         String lastName = "Doe";
+         System.out.println(firstName(+)lastName);
+         
+         //19
+         String txt = "Hello Everybody";
+         System.out.println(txt.indexoff(e));
+         
+         //20
+         int a = 5;
+         int b = 10;
+         Math.max(x,y);
+         
+         
+         //21
+         int c = 16;
+         Math.sqrt(c);
+         
+         //x22
+         Math.random(x);
+         
+         //23
+          boolean  isJavaFun = true;
+          boolean  isFishTasty = false;
+         System.out.println(isJavaFun);
+         System.out.println(isFishTasty);
+         
+         //24
+         int d = 10;
+         int e = 9;
+         System.out.println(d > e);
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+
+         
+        
+        
+        
+        
+    }
+    
+}
